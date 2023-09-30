2eaa3d7f4cca524ea32353b4a7542b3c48295792
Justin Huynh <justinhuynh639@gmail.com>
2023-09-30 01:50:19 -0700

    shuffle password function added

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ import random
 #take a list of characters we want to generate our password from
 characters == list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
+#create function that takes an input of how long the users want the password
 def generate_password():
     password_length = int(input("How long would you like your passworwd to be? "))
+
+    random.shuffle(characters)
+    #empty list that's going to hold the new password input
+    password = []
+    #use for loop to pick random charcters
+    for i in range(password_length):
+        password.append(random.choice(characters))
+
+    #shuffle the password
+    random.shuffle(password)
