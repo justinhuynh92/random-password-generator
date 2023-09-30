d705cdd570190f7cf93a1c37c6d821b0f3157885
Justin Huynh <justinhuynh639@gmail.com>
2023-09-30 01:57:47 -0700

    added print statements if y/n

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-#ask user if they want to generate a password or not
-#if yes, ask for password length
-#generate password
-#print password
-#if no, exit program
 import string
 import random
 
 #take a list of characters we want to generate our password from
-characters == list(string.ascii_letters + string.digits + "!@#$%^&*()")
+char == list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 #create function that takes an input of how long the users want the password
 def generate_password():
     password_length = int(input("How long would you like your passworwd to be? "))
 
-    random.shuffle(characters)
+    random.shuffle(char)
     #empty list that's going to hold the new password input
     password = []
     #use for loop to pick random charcters
     for i in range(password_length):
-        password.append(random.choice(characters))
+        password.append(random.choice(char))
 
     #shuffle the password
     random.shuffle(password)
+    #join password to blank quotes to assign to password variable
+    password = "".join(password)
+
+    print(password)
+
+#ask user
+option = input("Do you want to generate a password? (Yes/No)")
+#if yes, ask for password length
+if option == "Yes":
+    generate_password()
+#if no, exit program
+elif option == "No":
+    print("Program ended")
+    quit()
+#error handling if not yes or no
+else:
+    print("Invalid input, please input Yes or No")
+    quit()
